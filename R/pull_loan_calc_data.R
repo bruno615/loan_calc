f5c8719a6dd1874dce8cd34c9d996e367f892853
Mike Bruno <mike.bruno@avantcredit.com>
2015-01-13 16:48:21 -0600

    Modified output_directory

--- R/pull_loan_calc_data.R
+++ R/pull_loan_calc_data.R
@@ -13,17 +13,22 @@
 #' pull_loan_calc_data(1, "2015-01-10", output_directory = "~/query/output")
 #' @export
 
-pull_loan_calc_data <- function(loan_ids, sim_payoff_date, output_directory = "~/", file_identifier = "") {
+pull_loan_calc_data <- function(loan_ids, 
+                                sim_payoff_date = paste0("'", as.character(Sys.Date()),"'"), 
+                                output_directory = "~/", 
+                                file_identifier = "") {
   # Prep Loan IDs
   loan_ids <- paste(loan_ids,collapse = ",")
   loan_ids <- gsub("\n","",loan_ids)
+    #TODO  Format the payoff date - Add '
+    #TODO  Format output directory - add "/" if not already there
   # Query  
-    #To do, include these in the package and use from there
+    #TODO, include these in the package and use from there
   payoff_inflight <- run_query(pp(prep_sql_file("~/projects/queries/Loan Calc/R - Loan Calculator - DevOps Request - In Flight Lo.sql")))
   calc_loader <- run_query(pp(prep_sql_file("~/projects/queries/Loan Calc/R - Loan Calc - Loader Query.sql")))
   calc_pop <- run_query(pp(prep_sql_file("~/projects/queries/Loan Calc/R - Loan Calc - Loan Population Query V3.sql")))
   # Write to CSV  
-  write.csv(payoff_inflight, file = paste0(output_directory, "Loan Calc - Need Payoff Amounts ", sim_payoff_date, file_identifier,".csv"), row.names=FALSE, na="")
-  write.csv(calc_loader, file = paste0(output_directory, "Loan Calc - Loader ", sim_payoff_date, file_identifier,".csv"), row.names=FALSE, na="")  
-  write.csv(calc_pop, file = paste0(output_directory, "Loan Calc - Input ", sim_payoff_date, file_identifier,".csv"), row.names=FALSE, na="")  
+  write.csv(payoff_inflight, file = paste0(output_directory, "/Loan Calc - Need Payoff Amounts ", sim_payoff_date, file_identifier,".csv"), row.names=FALSE, na="")
+  write.csv(calc_loader, file = paste0(output_directory, "/Loan Calc - Loader ", sim_payoff_date, file_identifier,".csv"), row.names=FALSE, na="")  
+  write.csv(calc_pop, file = paste0(output_directory, "/Loan Calc - Input ", sim_payoff_date, file_identifier,".csv"), row.names=FALSE, na="")  
 }
